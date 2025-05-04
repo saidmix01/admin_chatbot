a21511171b07007b8f27d02269860e6f43031974
Said Avendano <saidavendano@artistshot.com>
2025-05-04 18:00:57 -0500

    module answer

--- js/questions/questions.js
+++ js/questions/questions.js
@@ -4,6 +4,10 @@ window.addEventListener('load', async function () {
 
 
 
+/**
+ * The `save_question` function in JavaScript handles saving a new question by sending data to a server
+ * and displaying success or error messages accordingly.
+ */
 const save_question = async () => {
     try {
         document.querySelector('.loading').style.display = "flex";
@@ -51,6 +55,14 @@ const save_question = async () => {
     }
 }
 
+/**
+ * The `load_questions` function fetches questions from a server, dynamically generates HTML elements
+ * for each question along with a form to submit answers, and displays them on the webpage.
+ * @param [que_parent=0] - The `que_parent` parameter in the `load_questions` function is used to
+ * specify the parent question ID for which you want to load the child questions. If `que_parent` is
+ * not provided or set to 0, it indicates that you want to load the top-level questions (questions
+ * without a
+ */
 const load_questions = async (que_parent = 0) => {
 	try {
 		document.querySelector('.loading').style.display = "flex";
@@ -119,6 +131,13 @@ const load_questions = async (que_parent = 0) => {
 	}
 }
 
+/**
+ * The `save_answer` function in JavaScript handles form submission, validation, sending data to a
+ * server, and displaying success or error messages to the user.
+ * @param [form] - The `form` parameter in the `save_answer` function is used to specify the form from
+ * which data will be saved. If the `form` parameter is not provided or is an empty string, an error
+ * "Form not found" will be thrown. The function then proceeds to extract data from the
+ */
 const save_answer = async (form="") => {
     try {
 		if(form=="") throw new Error("Form not found");
@@ -173,6 +192,18 @@ const save_answer = async (form="") => {
     }
 }
 
+/**
+ * The function `load_answer` asynchronously fetches answers for a given question ID and dynamically
+ * displays them on the webpage.
+ * @param [que_id] - The `load_answer` function is an asynchronous function that fetches answers for a
+ * specific question ID (`que_id`). It first checks if the `que_id` is provided, then makes a POST
+ * request to a specified URL to get the answers related to the provided question ID.
+ * @returns The `load_answer` function is returning the `html_answer` variable, which is a string
+ * containing HTML markup for a list of answers fetched from the server based on the provided `que_id`.
+ * The function makes an asynchronous request to fetch answers for a specific question, processes the
+ * response data to generate HTML markup for each answer, and then updates the DOM with the generated
+ * HTML content. Finally, it returns
+ */
 const load_answer = async (que_id = "") => {
 	let html_answer = "";
 	try {
