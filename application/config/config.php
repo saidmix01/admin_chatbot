ffa6e0ed78d91894d682883b2192bfa15eac61dd
Said Avendano <saidandresmix01@gmail.com>
2025-05-22 09:57:29 -0500

    upgrade to linux

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:5007/';
+$config['base_url'] = 'http://localhost/admin_chatbot/';
 
 /*
 |--------------------------------------------------------------------------
